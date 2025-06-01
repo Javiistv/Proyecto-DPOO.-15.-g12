4ee92f5500a99ea86dd840c46dea7a16b1ba7d63
Javiistv <javisotovillanueva@gmail.com>
2025-06-01 14:29:26 -0400

    Add files via upload

---
+++ prueba2/src/logic/Terminal.java
@@ -0,0 +1,105 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class Terminal {
+	
+	private ArrayList<Omnibus> parqueo;
+	private ArrayList<Conductor> conductores;
+	private ArrayList<Pasajero> pasajeros;
+	private ArrayList<Viaje> viajes;
+	
+	public ArrayList<Omnibus> getParqueo() {
+		return parqueo;
+	}
+	public ArrayList<Conductor> getConductores() {
+		return conductores;
+	}
+	public ArrayList<Pasajero> getPasajeros() {
+		return pasajeros;
+	}
+	public ArrayList<Viaje> getViajes() {
+		return viajes;
+	}
+	
+	public boolean addOmnibus(Omnibus om) {
+		boolean noExist = true;
+		for (int i = 0; i < parqueo.size() && noExist ; i++) {
+			if(om.getMatricula().equals(parqueo.get(i).getMatricula()))
+				noExist = false;
+		}
+		if(noExist)
+			this.parqueo.add(om);
+		
+		return noExist;
+	}
+	public boolean addConductor(Conductor cond) {
+		boolean noExist = true;
+		for (int i = 0; i < conductores.size() && noExist; i++){
+			if(cond.getNumID().equals(conductores.get(i).getNumID()))
+				noExist = false;
+		}
+		if(noExist)
+			conductores.add(cond);
+		
+		return noExist;
+	}
+	
+	public boolean addPasajero(Pasajero pass) {
+		boolean noExist = true;
+		for (int i = 0; i < pasajeros.size() && noExist; i++){
+			if(pasajeros.get(i).getNumID().equals(pass.getNumID()))
+				noExist = false;
+		}
+		if(noExist)
+			pasajeros.add(pass);
+		return noExist;
+	}
+	
+	public void setViajes(ArrayList<Viaje> viajes) {
+		this.viajes = viajes;
+	}
+	public Terminal() {
+		this.parqueo = new ArrayList<>();
+		this.conductores =new ArrayList<>();
+		this.pasajeros = new ArrayList<>();
+		this.viajes = new ArrayList<> ();
+	}
+
+	public ArrayList<Conductor> obtenerMayoresSalarios(){
+		ArrayList<Conductor> mayoresSalarios = new ArrayList<>();
+		double mayor = 0;
+		for(Conductor a: conductores)
+			if(a.calcularSalario() > mayor){
+				mayoresSalarios.clear();
+				mayor = a.calcularSalario();
+				mayoresSalarios.add(a);
+			}else
+				mayoresSalarios.add(a);		
+		
+		return mayoresSalarios;
+	}
+	
+	public Conductor buscarConductorPorId(String id) {
+		boolean encontro = false;
+		Conductor nuevo = null;
+		for (int i = 0; i < conductores.size() && !encontro ; i++){
+			if(conductores.get(i).getNumID().equals(id)){
+				encontro = true;
+			    nuevo = conductores.get(i);
+			}
+		}
+		return nuevo;    
+	}
+	
+	public Viaje buscarViajePorID(int id){
+		boolean encontro = false;
+		Viaje nuevo = null;
+		for(int i = 0; i < viajes.size() && !encontro; i++)
+			if(viajes.get(i).getId() == id){
+				encontro = true;
+				nuevo = viajes.get(i);
+			}
+		return nuevo;
+	}
+}
